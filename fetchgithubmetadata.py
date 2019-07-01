208bc425291e5e83f5d0e6ed41abce0b977a5201
Allen Mao <excursion.allen@gmail.com>
2019-07-01 15:13:14 -0700

    Added description and topics

--- fetchgithubmetadata.py
+++ fetchgithubmetadata.py
@@ -19,9 +19,14 @@ if url.netloc != 'github.com':
 _, owner, repo_name = url.path.split('/')
 print("Owner: {} Repo name: {}".format(owner, repo_name))
 general_resp = requests.get("https://api.github.com/repos/" + owner + "/" + repo_name).json()
-print(general_resp)
-keep_keys = ('name', 'owner', 'license', 'language', 'languages_url', 'releases_url')
+#print(general_resp)
+keep_keys = ('description', 'name', 'owner', 'license', 'language', 'languages_url', 'releases_url')
 filtered_resp = {k: general_resp[k] for k in keep_keys}
 filtered_resp['owner'] = filtered_resp['owner']['login']
+topics_headers = {
+    'Accept': 'application/vnd.github.mercy-preview+json',
+}
+topics_resp = requests.get('https://api.github.com/repos/' + owner + "/" + repo_name + '/topics', headers=topics_headers).json()
+filtered_resp['topics'] = topics_resp['names']
 resp_json = json.dumps(filtered_resp)
 print(resp_json)
