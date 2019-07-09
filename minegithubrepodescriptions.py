f61c4392d7a39fedba0e6e34c425981095befc8c
Allen Mao <excursion.allen@gmail.com>
2019-07-09 16:12:19 -0700

    Finished script that gathers brief repo descriptions & collected one megabyte of descriptions

---
+++ minegithubrepodescriptions.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# minegithubrepodescriptions.py
+
+import os
+import sys
+import requests
+import json
+import traceback
+from printprogressbar import printProgressBar
+
+output_corpus = 'data/repos_summary.csv'
+
+## read auth key
+auth2token_header = {}
+with open('config.json') as configfh:
+    auth2token_header = json.load(configfh)
+    configfh.close()
+
+try:
+    f = open(output_corpus, 'r+')
+    f.seek(0, os.SEEK_END)     # seek to end of file
+    while f.read(1) != "\n":   # Until EOL is found...
+        f.seek(f.tell() - 2, os.SEEK_SET) # ...jump back the read byte plus one more.
+        #f.seek(-2, 1)
+    last = f.readline()         # Read last line.
+    id, _ = [x.strip() for x in last.split(',')]
+    f.seek(0, os.SEEK_END)
+except (FileNotFoundError, ValueError):
+    # FileNotFoundError: file does not exist yet
+    # ValueError: no downloaded entries yet
+    # restart from scratch: create or overwrite existing file
+    traceback.print_exc()
+    print("Creating file...")
+    f = open(output_corpus, 'w')
+    f.write("id, description")
+    id = '0'
+
+requests_remaining = 1
+while requests_remaining > 0:
+    reporequest = requests.get("https://api.github.com/repositories", params={'since':id}, headers=auth2token_header)
+    requests_remaining = int(reporequest.headers['X-RateLimit-Remaining'])
+    total_requests = int(reporequest.headers['X-RateLimit-Limit'])
+
+    for repo in reporequest.json():
+        repo_id = repo['id']
+        repo_description = repo['description']
+        print("ID: {}, Description: {}".format(repo_id, repo_description))
+        if (repo_description):
+            f.write("\n{},{}".format(repo_id, repo_description))
+        printProgressBar(total_requests-requests_remaining, total_requests, prefix = '% of used requests', suffix = 'done')
+    id = repo_id
