7b0816af2f0d48ee19c37378a32fabe82d2739ab
Allen Mao <excursion.allen@gmail.com>
2019-07-09 23:05:35 -0700

    Updated minegithubrepodescriptions.py: add quotation marks to separate csv entries; ran script and collected ~440 000 repo summary descriptions

--- minegithubrepodescriptions.py
+++ minegithubrepodescriptions.py
@@ -6,6 +6,7 @@ import sys
 import requests
 import json
 import traceback
+import csv
 from printprogressbar import printProgressBar
 
 output_corpus = 'data/repos_summary.csv'
@@ -23,7 +24,8 @@ try:
         f.seek(f.tell() - 2, os.SEEK_SET) # ...jump back the read byte plus one more.
         #f.seek(-2, 1)
     last = f.readline()         # Read last line.
-    id, _ = [x.strip() for x in last.split(',')]
+    id, _ = [x.strip('\"') for x in last.split(',')]
+    id = int(id)
     f.seek(0, os.SEEK_END)
 except (FileNotFoundError, ValueError):
     # FileNotFoundError: file does not exist yet
@@ -32,20 +34,24 @@ except (FileNotFoundError, ValueError):
     traceback.print_exc()
     print("Creating file...")
     f = open(output_corpus, 'w')
-    f.write("id, description")
-    id = '0'
-
+    csv_writer = csv.writer(f, dialect='unix')
+    csv_writer.writerow(['id', 'description'])
+    id = 0
+else:
+    csv_writer = csv.writer(f, dialect='unix')
 requests_remaining = 1
 while requests_remaining > 0:
+#    print(id)
     reporequest = requests.get("https://api.github.com/repositories", params={'since':id}, headers=auth2token_header)
+#    print(reporequest.request)
     requests_remaining = int(reporequest.headers['X-RateLimit-Remaining'])
     total_requests = int(reporequest.headers['X-RateLimit-Limit'])
-
+    #print(reporequest.json())
     for repo in reporequest.json():
         repo_id = repo['id']
         repo_description = repo['description']
         print("ID: {}, Description: {}".format(repo_id, repo_description))
         if (repo_description):
-            f.write("\n{},{}".format(repo_id, repo_description))
+            csv_writer.writerow([repo_id, repo_description])
         printProgressBar(total_requests-requests_remaining, total_requests, prefix = '% of used requests', suffix = 'done')
     id = repo_id
