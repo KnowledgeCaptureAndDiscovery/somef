75842afc62f8f0fdbbbd95abb84598d445d0fa05
Allen Mao <excursion.allen@gmail.com>
2019-07-15 22:04:42 -0700

    training.py: added more insightful metrics. testing.py: started beginning. data/repos_summary.csv: added some more entries

--- testing.py
+++ testing.py
@@ -1,1 +1,11 @@
 # testing.py
+# parameters:
+## input file: either: url to github repository OR markdown documentation file path
+## output file: json with each paragraph marked with all four classification scores
+
+import argparse
+
+auth2token_header = {}
+with open('config.json') as fh:
+    auth2token_header = json.load(fh)
+
