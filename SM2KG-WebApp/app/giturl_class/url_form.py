66bb60deca08f8e685ebaeb84b02f35058eeef04
vmdiwanji <54455062+vmdiwanji@users.noreply.github.com>
2020-03-20 01:45:56 -0700

    Removing Web App from cli

--- SM2KG-WebApp/app/giturl_class/url_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class UrlForm(FlaskForm):
-    giturl = StringField('Git URL', validators=[DataRequired()])
-    threshold = StringField('Classifier Threshold', validators=[DataRequired()])
-    submit = SubmitField('Classify')
