e3ce9ef6060ddcd4889dec4e13d4732a46ba5049
Joseph <lee883@usc.edu>
2020-03-24 01:31:26 -0700

    updated website

--- SM2KG-WebApp/app/giturl_class/url_form.py
+++ SM2KG-WebApp/app/giturl_class/url_form.py
@@ -3,7 +3,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class UrlForm(FlaskForm):
-    giturl = StringField('Git URL', validators=[DataRequired()])
-    threshold = StringField('Classifier Threshold', validators=[DataRequired()], render_kw={"placeholder": "Default value: .80"})
-
+    giturl = StringField('Git URL', validators=[DataRequired()], render_kw={"placeholder": "Enter GitHub URL"});
+    threshold = StringField('Classifier Threshold', render_kw={"placeholder": "Default value: .80"})
     submit = SubmitField('Classify')
