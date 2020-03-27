258a24aa6582ac1f093ee571b5cee0294debace2
tommy eblen <tommyeblen@gmail.com>
2020-03-27 03:59:41 -0700

    some changes

--- sm2kg/setup.py
+++ sm2kg/setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup
 
 setup(name='sm2kg',
-        version='0.1',
+        version='0.2',
         description='test desc',
         packages=['sm2kg'],
+        install_requires=[
+            'numpy',
+            'pandas',
+            'sklearn',
+            'nltk',
+            'matplotlib',
+            'bs4',
+            'requests',
+            'markdown',
+        ],
         include_package_data=True,
         zip_safe=False)
