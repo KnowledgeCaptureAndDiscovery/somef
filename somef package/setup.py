0c51341742fc2ddc8b09f956201fee1f8163c24d
vmdiwanji <54455062+vmdiwanji@users.noreply.github.com>
2020-04-10 15:22:54 -0700

    pip package source files

---
+++ somef package/setup.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import find_packages, setup
+
+install_requires = [
+    "Click",
+    "requests",
+    "markdown",
+    "requests",
+    "bs4",
+    "matplotlib",
+    "nltk",
+    "sklearn",
+    "pandas",
+    "numpy"
+]
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def find_package_data(dirname):
+    def find_paths(dirname):
+        items = []
+        for fname in os.listdir(dirname):
+            path = os.path.join(dirname, fname)
+            if os.path.isdir(path):
+                items += find_paths(path)
+            elif not path.endswith(".py") and not path.endswith(".pyc"):
+                items.append(path)
+        return items
+
+    items = find_paths(dirname)
+    return [os.path.relpath(path, dirname) for path in items]
+
+
+version = {}
+with open("src/somef/__init__.py") as fp:
+    exec(fp.read(), version)
+
+
+setup(
+    name="somef",
+    version=version["__version__"],
+    author="Vedant Diwanji",
+    author_email="vedant.diwanji@gmail.com",
+    description=__doc__,
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KnowledgeCaptureAndDiscovery/SM2KG",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Unix",
+    ],
+    entry_points={"console_scripts": ["somef = somef.__main__:trycli"]},
+    package_dir={"": "src"},
+    packages=find_packages(where="src", exclude=["somef.tests*"]),
+    package_data={"somef": find_package_data("src/somef")},
+    exclude_package_data={"somef": ["tests/*"]},
+    zip_safe=False,
+    install_requires=install_requires,
+    python_requires=">=3.6.0",
+)
