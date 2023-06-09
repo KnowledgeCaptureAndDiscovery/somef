d5fd353e6e5b74edefaed12ede6ef08cab9e9a67
rafaeel9 <115452767+rafaeel9@users.noreply.github.com>
2023-06-09 08:11:01 +0000

    Updated function to past tests.

--- src/somef/extract_workflows.py
+++ src/somef/extract_workflows.py
@@ -2,6 +2,7 @@ import re
 Galaxy_pattern = r"(?i)a[_\s-]?galaxy[_\s-]?workflow"
 CWL_pattern = r"\bclass:\s*[Ww]orkflow\b"
 Workflow_content_pattern = r"in:\s*[^}]*\s*out:\s*(?:\[.*?\]|.*?(?=\n\s*\S+:|$))"
+workflow_pattern=r'\bworkflow\b'
 Nextflow_pattern= r"(?i)nextflow[\s\S]*?(workflow\s*\{[\s\S]*?\})"
 
 def is_file_workflow(file_path):
@@ -11,8 +12,9 @@ def is_file_workflow(file_path):
             Galaxy_match=re.search(Galaxy_pattern,content)
             CWL_match=re.search(CWL_pattern,content)
             Workflow_match=re.search(Workflow_content_pattern,content)
+            Workflow_match_2=re.search(workflow_pattern,content,re.IGNORECASE)
             Nextflow_match=re.search(Nextflow_pattern,content)
-            if Galaxy_match or CWL_match or Workflow_match or Nextflow_match:
+            if Galaxy_match or CWL_match or Workflow_match or Workflow_match_2 or Nextflow_match:
                 return True
             else:
                 return False
--- src/somef/test/test_extract_workflows.py
+++ src/somef/test/test_extract_workflows.py
@@ -10,7 +10,7 @@ class TestWorkflows(unittest.TestCase):
     def test_is_workflow(self):
         
         workflow = extract_workflows.is_file_workflow(test_data_repositories + "SimulatedReads2Map.wdl")
-        assert workflow, "The file does not contain a workflow."
+        assert workflow, "The file does contain a workflow."
 
     def test_is_workflow_fake(self):
         workflow = extract_workflows.is_file_workflow(test_data_repositories + "repositories/wav2letter/scripts/arrayfire_parser.py")
