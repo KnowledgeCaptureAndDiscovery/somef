8ce43071a8179eaa522bf31311ef5b14cfd86113
Anas-Elhounsri <elhounsri.anas@gmail.com>
2025-05-05 12:35:41 +0200

    somef now detects and parses metadata of Cargo.toml

--- src/somef/process_files.py
+++ src/somef/process_files.py
@@ -14,6 +14,7 @@ from .parser.python_parser import parse_pyproject_toml
 from .parser.python_parser import parse_setup_py
 from.parser.python_parser import parse_requirements_txt
 from .parser.codemeta_parser import parse_codemeta_json_file
+from .parser.cargo_parser import parse_cargo_toml
 from chardet import detect
 
 domain_gitlab = ''
@@ -182,9 +183,10 @@ def process_repository_files(repo_dir, metadata_result: Result, repo_type, owner
                     codemeta_file_url = get_file_link(repo_type, file_path, owner, repo_name, repo_default_branch, repo_dir, repo_relative_path, filename)
                     metadata_result = parse_codemeta_json_file(os.path.join(dir_path, filename), metadata_result, codemeta_file_url)
                     # TO DO: Code owners not fully implemented yet
-                
+            
                 if filename.lower() == "pom.xml" or filename.lower() == "package.json" or \
-                    filename.lower() == "pyproject.toml" or filename.lower() == "setup.py" or filename.lower() == "requirements.txt":
+                    filename.lower() == "pyproject.toml" or filename.lower() == "setup.py" or \
+                    filename.lower() == "requirements.txt" or (filename.lower() == "cargo.toml" and repo_relative_path == "."):
                         build_file_url = get_file_link(repo_type, file_path, owner, repo_name, repo_default_branch,
                                                        repo_dir,
                                                        repo_relative_path, filename)
@@ -207,6 +209,8 @@ def process_repository_files(repo_dir, metadata_result: Result, repo_type, owner
                             metadata_result = parse_setup_py(os.path.join(dir_path, filename), metadata_result, build_file_url)
                         if filename.lower() == "requirements.txt":
                             metadata_result = parse_requirements_txt(os.path.join(dir_path, filename), metadata_result, build_file_url)
+                        if filename.lower() == "cargo.toml":
+                            metadata_result = parse_cargo_toml(os.path.join(dir_path, filename), metadata_result, build_file_url)
 
                 # if repo_type == constants.RepositoryType.GITLAB: 
                 if filename.endswith(".yml"):
