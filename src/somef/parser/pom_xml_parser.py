3e60c0d038ae9fad716939a1acc0173dba0e64f4
Juanje Mendoza <juanjemd@gmail.com>
2025-11-14 15:05:59 +0100

    Changes in the parsers made during the documentation process due to inconsistencies or errors

--- src/somef/parser/pom_xml_parser.py
+++ src/somef/parser/pom_xml_parser.py
@@ -40,6 +40,7 @@ def parse_pom_file(file_path, metadata_result: Result, source):
         data = lxml.etree.parse(file)
         
         root = data.getroot()
+
         if root.tag != "{" + POM_NAMESPACE + "}project":
             logging.warning(f"Expected root tag 'project' in {POM_NAMESPACE} namespace, got {root.tag} instead")
             return metadata_result
@@ -83,8 +84,14 @@ def parse_pom_file(file_path, metadata_result: Result, source):
                 project_data["scm_url"] = parse_scm(node)        
             elif key == "developers":
                 project_data["developers"] = parse_developers(node)              
+            # elif key == "dependencies" or key == "dependencyManagement":
+            #     project_data["dependencies"] = parse_dependencies(node)
             elif key == "dependencies":
-                project_data["dependencies"] = parse_dependencies(node)
+                project_data["dependencies"].extend(parse_dependencies(node))
+            elif key == "dependencyManagement":
+                for k, n in parse_node(node):
+                    if k == "dependencies":
+                        project_data["dependencies"].extend(parse_dependencies(n))
             elif key == "properties":
                 project_data["runtime_platform"] = parse_runtime_platform(node)
 
@@ -147,7 +154,7 @@ def parse_pom_file(file_path, metadata_result: Result, source):
                 constants.TECHNIQUE_CODE_CONFIG_PARSER,
                 source
             )
-
+ 
         if project_data["dependencies"]:
             for dependency in project_data["dependencies"]:
                 metadata_result.add_result(
@@ -179,23 +186,37 @@ def parse_pom_file(file_path, metadata_result: Result, source):
                 )
 
         if repositories:
-            metadata_result.add_result(
-                constants.CAT_PACKAGE_DISTRIBUTION,
-                {
-                    "value": repositories,
-                    "type": constants.URL
-                },
-                1,
-                constants.TECHNIQUE_CODE_CONFIG_PARSER,
-                source
-            )
 
+            for rep in repositories:
+                rep["type"] = constants.URL
+
+                metadata_result.add_result(
+                    constants.CAT_PACKAGE_DISTRIBUTION,
+                    rep,
+                    1,
+                    constants.TECHNIQUE_CODE_CONFIG_PARSER,
+                    source
+                )
+
+                #     constants.CAT_PACKAGE_DISTRIBUTION,
+                #     {
+                #         # "value": repositories,
+                #         rep,
+                #         "type": constants.URL
+                #     },
+                #     1,
+                #     constants.TECHNIQUE_CODE_CONFIG_PARSER,
+                #     source
+                # )
+          
         if project_data["runtime_platform"]:
+  
             for runtime in project_data["runtime_platform"]:
                 metadata_result.add_result(
                     constants.CAT_RUNTIME_PLATFORM,
                     {
-                        "value": runtime["version"],
+                        "value": runtime["value"],
+                        "version": runtime["version"],
                         "name": runtime["name"],
                         "type": constants.STRING
                     },
@@ -221,7 +242,6 @@ def parse_pom_file(file_path, metadata_result: Result, source):
         )
         
         processed_pom = True
-
     return metadata_result
 
 def parse_licenses(licenses_node):
@@ -236,16 +256,27 @@ def parse_licenses(licenses_node):
                 licenses.append(license_data)
     return licenses
 
-def parse_dependencies(dependencies_node):
+def parse_dependencies(node):
     dependencies = []
-    for key, node in parse_node(dependencies_node):
-        if key == "dependency":
+
+    for child in node:
+        if not isinstance(child.tag, str):
+            continue
+
+        tag = child.tag.split("}")[-1] 
+        if tag == "dependencies":
+            dependencies.extend(parse_dependencies(child))
+        elif tag == "dependency":
             dep_data = {}
-            for key2, node2 in parse_node(node):
-                if key2 in ["groupId", "artifactId", "version"] and node2.text:
-                    dep_data[key2] = node2.text
+            for sub in child:
+                if not isinstance(sub.tag, str):
+                    continue 
+                sub_tag = sub.tag.split("}")[-1]
+                if sub_tag in ["groupId", "artifactId", "version"] and sub.text:
+                    dep_data[sub_tag] = sub.text.strip()
             if dep_data:
                 dependencies.append(dep_data)
+
     return dependencies
 
 def parse_developers(developers_node):
@@ -296,7 +327,10 @@ def parse_repositories(repo_node):
             repo_data = {}
             for key2, node2 in parse_node(node):
                 if key2 in ["id", "name", "url"] and node2.text:
-                    repo_data[key2] = node2.text
+                    if key2 == "id":
+                        repo_data["value"] = node2.text
+                    else:
+                        repo_data[key2] = node2.text
             if repo_data:
                 repos.append(repo_data)
     return repos
@@ -323,10 +357,35 @@ def parse_runtime_platform(properties_node):
         return runtimes
 
     for child in properties_node:
-        tag = child.tag
-        if tag.startswith(f"{{{POM_NAMESPACE}}}") and tag.endswith(".version") and child.text:
-            runtime_name = tag.split("}")[-1].split(".")[0].capitalize()
-            version_value = child.text.strip()
-            runtimes.append({"name": runtime_name, "version": version_value})
+        if not isinstance(child.tag, str):
+            continue
+        tag = child.tag.split("}")[-1].lower()
+        text = (child.text or "").strip()
+
+        if not text:
+            continue
+        # if tag.startswith(f"{{{POM_NAMESPACE}}}") and tag.endswith(".version") and child.text:
+        #     print('entramos')
+        #     runtime_name = tag.split("}")[-1].split(".")[0].capitalize()
+        #     version_value = child.text.strip()
+        #     runtimes.append({"value": f'{runtime_name} {version_value}',"name": runtime_name, "version": version_value})
+        if any(x in tag for x in ["java.version", "javaversion", "java_version"]):
+            runtimes.append({
+                "name": "Java",
+                "version": text,
+                "value": f"Java: {text}"
+            })
+        elif any(x in tag for x in ["kotlin.version", "kotlinversion", "kotlin_version"]):
+            runtimes.append({
+                "name": "Kotlin",
+                "version": text,
+                "value": f"Kotlin: {text}"
+            })
+        elif any(x in tag for x in ["scala.version", "scalaversion", "scala_version"]):
+            runtimes.append({
+                "name": "Scala",
+                "version": text,
+                "value": f"Scala: {text}"
+            })
     
     return runtimes
