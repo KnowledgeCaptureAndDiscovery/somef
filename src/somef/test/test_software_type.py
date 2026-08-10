9a60b6caf1ed4c2ac8c810a8e1d30057bd007cdd
Juanje Mendoza <juanjemd@gmail.com>
2026-08-10 15:54:08 +0200

    better application type. More tests. Fix #963

--- src/somef/test/test_software_type.py
+++ src/somef/test/test_software_type.py
@@ -3,7 +3,9 @@ import os
 from pathlib import Path
 
 from ..extract_software_type import check_ontologies, check_notebooks, check_command_line, check_extras, \
-    check_static_websites, check_workflow
+    check_static_websites, check_workflow, check_package_files, check_repository_type
+from ..process_results import Result
+from ..utils import constants
 
 test_data_repositories = str(Path(__file__).parent / "test_data" / "repositories") + os.path.sep
 
@@ -64,3 +66,66 @@ class TestEXTRAS(unittest.TestCase):
         path = test_data_repositories + "A-Dynamic-E-Commerce-Website-master"
         result = check_workflow(path, 'DynamicPersonalWebsite-master')
         assert result is False
+
+    def test_true_package_files(self):
+        path = test_data_repositories + "fuji"
+        result = check_package_files(path)
+        assert result
+
+    def test_false_package_files(self):
+        path = test_data_repositories + "OWL-To-OAS-Specification-master"
+        result = check_package_files(path)
+        assert result is False
+
+    def test_repository_type_software(self):
+        path = test_data_repositories + "fuji"
+        result = check_repository_type(path, "fuji", Result())
+        values = [r[constants.PROP_RESULT][constants.PROP_VALUE] for r in result.results[constants.CAT_APPLICATION_TYPE]]
+        assert "software" in values
+
+    def test_repository_type_non_software(self):
+        path = test_data_repositories + "OWL-To-OAS-Specification-master"
+        result = check_repository_type(path, "OWL-To-OAS-Specification-master", Result())
+        values = [r[constants.PROP_RESULT][constants.PROP_VALUE] for r in result.results[constants.CAT_APPLICATION_TYPE]]
+        assert "non-software" in values
+
+    def test_repository_type_ontology(self):
+        path = test_data_repositories + "auroral-ontology-core"
+        result = check_repository_type(path, "auroral-ontology-core", Result())
+        values = [r[constants.PROP_RESULT][constants.PROP_VALUE] for r in result.results[constants.CAT_APPLICATION_TYPE]]
+        assert "ontology" in values
+
+    def test_true_static_website(self):
+        path = test_data_repositories + "website-static-master"
+        result = check_static_websites(path, Result())
+        assert result
+
+    def test_false_static_website(self):
+        path = test_data_repositories + "rdflib-6.0.2"
+        result = check_static_websites(path, Result())
+        assert result is False
+
+    def test_high_js_ratio_should_be_false(self):
+        path = test_data_repositories + "website-static-master"
+        repo_metadata = Result()
+        repo_metadata.results = {
+            constants.CAT_PROGRAMMING_LANGUAGES: [
+                {constants.PROP_RESULT: {constants.PROP_NAME: "javascript", constants.PROP_SIZE: 95}},
+                {constants.PROP_RESULT: {constants.PROP_NAME: "html", constants.PROP_SIZE: 5}},
+            ]
+        }
+        result = check_static_websites(path, repo_metadata)
+        assert result is False
+
+
+    def test_low_js_ratio_should_be_true(self):
+        path = test_data_repositories + "website-static-master"
+        repo_metadata = Result()
+        repo_metadata.results = {
+            constants.CAT_PROGRAMMING_LANGUAGES: [
+                {constants.PROP_RESULT: {constants.PROP_NAME: "javascript", constants.PROP_SIZE: 5}},
+                {constants.PROP_RESULT: {constants.PROP_NAME: "html", constants.PROP_SIZE: 95}},
+            ]
+        }
+        result = check_static_websites(path, repo_metadata)
+        assert result is True
